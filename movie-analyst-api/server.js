ac2d822529b7e4c651738d2aa3d83ed37b8df315
valentina siabatto <valesiabatto@gmail.com>
2020-09-11 13:18:04 -0500

    dockerfile back

--- movie-analyst-api/server.js
+++ movie-analyst-api/server.js
@@ -49,7 +49,7 @@ app.get('/publications', async function (req, res) {
 app.get('/pending', async function (req, res) {
   try {
     const rows = await pool.query(
-      'select m.title, m.release_year, m.score, r.name as reviewer, p.name as publication from movie_db.movies m, movie_db.reviewers r, movie_db.publications p where r.publication=p.name and m.reviewer=r.name and m.release_year>=2017'
+      'select m.title, m.release_year, m.score, r.name as reviewer, p.name as publication from movies m, reviewers r, publications p where r.publication=p.name and m.reviewer=r.name and m.release_year>=2017'
     )
     res.json(rows)
   } catch (err) {
